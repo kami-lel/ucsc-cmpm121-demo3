2b9648a720d2214612cf6415b4223766a9088804
kami lel <77594253+kami-lel@users.noreply.github.com>
2024-12-03 01:23:21 -0800

    add game.ts

--- src/example.ts
+++ src/example.ts
@@ -1,12 +1,4 @@
-// @deno-types="npm:@types/leaflet@^1.9.14"
-import leaflet from "leaflet";
-
-// Style sheets
-import "leaflet/dist/leaflet.css";
-import "./style.css";
-
-// Fix missing marker images
-import "./leafletWorkaround.ts";
+// FIXME rm
 
 // Deterministic random number generator
 import luck from "./luck.ts";
@@ -14,40 +6,13 @@ import luck from "./luck.ts";
 // Location of our classroom (as identified on Google Maps)
 const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
 
-// Tunable gameplay parameters
-const GAMEPLAY_ZOOM_LEVEL = 19;
-const TILE_DEGREES = 1e-4;
-const NEIGHBORHOOD_SIZE = 8;
-const CACHE_SPAWN_PROBABILITY = 0.1;
-
-// Create the map (element with id "map" is defined in index.html)
-const map = leaflet.map(document.getElementById("map")!, {
-  center: OAKES_CLASSROOM,
-  zoom: GAMEPLAY_ZOOM_LEVEL,
-  minZoom: GAMEPLAY_ZOOM_LEVEL,
-  maxZoom: GAMEPLAY_ZOOM_LEVEL,
-  zoomControl: false,
-  scrollWheelZoom: false,
-});
-
-// Populate the map with a background tile layer
-leaflet
-  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  })
-  .addTo(map);
-
-// Add a marker to represent the player
-const playerMarker = leaflet.marker(OAKES_CLASSROOM);
-playerMarker.bindTooltip("That's you!");
-playerMarker.addTo(map);
 
 // Display the player's points
 let playerPoints = 0;
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!; // element `statusPanel` is defined in index.html
 statusPanel.innerHTML = "No points yet...";
+
+
 
 // Add caches to the map by cell numbers
 function spawnCache(i: number, j: number) {
